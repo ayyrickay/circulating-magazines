3014e4dc1b18281f3c6eeef7c0c7a8151e8bc191
Ricky Holtz <evholtz@gmail.com>
2018-07-06 22:08:10 -0700

    Working maps

--- assets/js/index.js
+++ assets/js/index.js
@@ -65,13 +65,15 @@ const stateCodes = {
   'Wyoming': 'WY'
 }
 
-const filterByMagazine = (sourceGroup, magazine) => {
+const repairGeoKey = (sourceGroup) => {
     return {
         all: () => {
-            return sourceGroup.all().filter((d) => {
+            return sourceGroup.all().map((d) => {
+              console.log()
               const dimensions = d.key.split(',')
-              d.key = dimensions[1]
-              return dimensions[0] === magazine
+              d.key = dimensions[0]
+              if(dimensions[0] === 'Maine') console.log(d)
+              return d
             });
         }
     }
@@ -80,11 +82,12 @@ const filterByMagazine = (sourceGroup, magazine) => {
 const filterCirculationByMagazine = (sourceGroup, magazine) => {
     return {
         all: () => {
+          console.log('sourcegroup', sourceGroup.all())
             return sourceGroup.all().filter((d) => {
-              console.log('BEFORE', d)
+              // console.log('BEFORE', d)
               const dimensions = d.key ? d.key.split(',') : [null, null]
               d.key = dimensions[1]
-              console.log('AFTER', d)
+              // console.log('AFTER', d)
               return dimensions[0] === magazine
             });
         }
@@ -94,22 +97,41 @@ const filterCirculationByMagazine = (sourceGroup, magazine) => {
 // const industryChart = dc.bubbleChart("#industry-chart")
 // const roundChart = dc.bubbleChart("#round-chart")
 
-Promise.all(sources.map(url => d3.json(url)))
-.then((data) => {
-    const geodata = crossfilter(data[0])
-    const circulation = crossfilter(data[1])
-    const states = geodata.dimension(d => d.State)
-    const chart1Key = geodata.dimension((d) => `${d["Title"]},${d["State/Region"]}`)
-    const chart2Key = geodata.dimension((d) => `${d["Title"]},${d["State/Region"]}`)
-    const chart1Group = chart1Key.group().reduceSum((d) => d["Total"])
-    const chart2Group = chart2Key.group().reduceSum((d) => d["Total"])
+// Promise.all(sources.map(url => d3.json(url)))
+// .then((data) => {
+//     const geodata = crossfilter(data[0])
+//     const circulation = crossfilter(data[1])
+//     const states = geodata.dimension(d => d.State)
+//     const chart1Key = geodata.dimension((d) => `${d["Title"]},${d["State/Region"]}`)
+//     const chart2Key = geodata.dimension((d) => `${d["Title"]},${d["State/Region"]}`)
 
-    d3.json("./assets/geo/us-states.json").then(function (statesJson) {
+d3.json("./assets/data/joined_data.json").then((data) => {
+
+  const title1Data = data.filter((d) => {
+    return d.magazine_title === 'New Yorker'
+  })
+
+  const title2Data = data.filter((d) => {
+    return d.magazine_title === 'Saturday Evening Post'
+  })
+
+  const crossfilter = crossfilter(data)
+
+  const title1Circulation = crossfilter(title1Data)
+  const title1States = title1Circulation.dimension(d => d.state_region)
+  const chart1Key = title1Circulation.dimension((d) => `${d.state_region},${d.sampled_issue_date}`)
+  const chart1Group = chart1Key.group().reduceSum(d => d.sampled_total_sales)
+
+  const title2Circulation = crossfilter(title2Data)
+  const title2States = title2Circulation.dimension(d => d.state_region)
+  const chart2Key = title2Circulation.dimension((d) => `${d.state_region}, ${d.sampled_issue_date}`)
+  const chart2Group = chart2Key.group().reduceSum(d => d.sampled_total_sales)
+    d3.json("./assets/geo/us-states.json").then((statesJson) => {
         console.log("loading state json")
-        us1Chart.dimension(states)
-                .group(filterByMagazine(chart1Group, "New Yorker"))
+        us1Chart.dimension(title1States)
+                .group(repairGeoKey(chart1Group))
                 .colors(d3.scaleQuantize().range(["#E2F2FF", "#C4E4FF", "#9ED2FF", "#81C5FF", "#6BBAFF", "#51AEFF", "#36A2FF", "#1E96FF", "#0089FF", "#0061B5"]))
-                .colorDomain([0, 50000])
+                .colorDomain([-1, 100000])
                 .colorCalculator(function (d) { return d ? us1Chart.colors()(d) : '#ccc' })
                 .overlayGeoJson(statesJson.features, "state", function (d) {
                     return d.properties.name
@@ -119,15 +141,15 @@ Promise.all(sources.map(url => d3.json(url)))
                   .translate([document.getElementById('us2-chart').offsetWidth / 2, document.getElementById('us2-chart').offsetHeight / 2])
                 )
                 .valueAccessor(function(kv) {
-                    console.log(kv)
+                    // console.log(kv)
                     return kv.value
                 })
                 .title(function (d) {
                     return "State: " + d.key + "\nCirculation Total: " + d.value ? d.value : 0
                 })
 
-        us2Chart.dimension(states)
-                .group(filterByMagazine(chart2Group, "Saturday Evening Post"))
+        us2Chart.dimension(title2States)
+                .group(repairGeoKey(chart2Group))
                 .colors(d3.scaleQuantize().range(["#E2F2FF", "#C4E4FF", "#9ED2FF", "#81C5FF", "#6BBAFF", "#51AEFF", "#36A2FF", "#1E96FF", "#0089FF", "#0061B5"]))
                 .colorDomain([0, 1000000])
                 .colorCalculator(function (d) { return d ? us2Chart.colors()(d) : '#ccc' })
@@ -139,17 +161,16 @@ Promise.all(sources.map(url => d3.json(url)))
                   .translate([document.getElementById('us2-chart').offsetWidth / 2, document.getElementById('us2-chart').offsetHeight / 2])
                 )
                 .valueAccessor(function(kv) {
-                    console.log(kv)
+                    // console.log(kv)
                     return kv.value
                 })
                 .title(function (d) {
                     return "State: " + d.key + "\nCirculation Total: " + d.value ? d.value : 0
                 })
 
-        const all = circulation.groupAll()
-        const lineChartYear = circulation.dimension(d => d.year)
-        const lineChartYear1 = circulation.dimension(d => `${d.magazine},${d.year}`)
-        const lineChartYear2 = circulation.dimension(d => `${d.magazine},${d.year}`)
+        const lineChartDate = title1Circulation.dimension(d => d.actual_issue_date)
+        const lineChartYear1 = title1Circulation.dimension(d => d.actual_issue_date)
+        const lineChartYear2 = title2Circulation.dimension(d => d.actual_issue_date)
 
         const circulationGroup1 = lineChartYear1.group().reduceSum(d => d.circulation)
         const circulationGroup2 = lineChartYear2.group().reduceSum(d => d.circulation)
@@ -157,7 +178,7 @@ Promise.all(sources.map(url => d3.json(url)))
         lineChart.width(1160)
             .height(250)
             .margins({ top: 10, right: 10, bottom: 20, left: 40 })
-            .dimension(lineChartYear)
+            .dimension(lineChartDate)
             .transitionDuration(500)
             .elasticY(true)
             .brushOn(false)
@@ -165,13 +186,13 @@ Promise.all(sources.map(url => d3.json(url)))
                 return d.value;
             })
             .title(function (d) {
-                return "\nNumber of Povetry: " + d.key;
+                return "\nCirculation: " + d.key;
 
             })
             .x(d3.scaleTime().domain([1925, 2001]))
             .compose([
-                dc.lineChart(lineChart).group(filterCirculationByMagazine(circulationGroup1, "New Yorker")),
-                dc.lineChart(lineChart).group(filterCirculationByMagazine(circulationGroup2, "Saturday Evening Post"))
+                dc.lineChart(lineChart).group(circulationGroup1),
+                dc.lineChart(lineChart).group(circulationGroup2)
             ])
 
         dc.renderAll()
