b03bad1bbcd33f75ee1616d2ef80c0c032676be6
Ricky Holtz <evholtz@gmail.com>
2019-01-31 10:35:40 -0800

    Use file name for dropdown generator

--- assets/data/scripts/list-of-title-generator.js
+++ assets/data/scripts/list-of-title-generator.js
@@ -1,19 +1,19 @@
 const path = require('path')
 const fs = require('fs')
 
-const titles = fs.readdirSync('./clean').filter(file => file.indexOf('geodata') > -1)
+const titles = fs.readdirSync('../clean').filter(file => file.indexOf('circulation') > -1)
 console.log(titles)
 
 const data = []
 
 for (let i = 0; i < titles.length; i++) {
-  const json = JSON.parse(fs.readFileSync(`./clean/${titles[i]}`))
+  const json = JSON.parse(fs.readFileSync(`../clean/${titles[i]}`))
+  const title_code = titles[i].split('-')[0]
   const title = json[5]
 
   if (title) {
-    console.log(title.magazine_title, title.title_code)
-    const names = title.magazine_title.split('@')
-    names.forEach(name => data.push({value: title.title_code.toLowerCase(), label: name}))
+    const name = title.canonical_title
+    data.push({value: title_code.toLowerCase(), label: name})
   } else {
     console.log('magazine title is undefined for', json[4])
   }
@@ -25,7 +25,7 @@ for (let i = 0; i < titles.length; i++) {
 console.log(data)
 console.log('length of data is', data.length)
 
-fs.writeFile('clean/titles-list.json', JSON.stringify(data), 'utf8', (err) => {
+fs.writeFile(path.join(__dirname, '/../clean/titles-list.json'), JSON.stringify(data), 'utf8', (err) => {
   if (err) throw err
   console.log('Successfully joined data')
 })
