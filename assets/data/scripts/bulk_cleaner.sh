276422ab6501c5ac4c394950eaa674ff3f2d1b92
Ricky <evholtz@gmail.com>
2019-05-20 00:25:20 -0600

    Add five new titles (#58)

--- assets/data/scripts/bulk_cleaner.sh
+++ assets/data/scripts/bulk_cleaner.sh
@@ -1,11 +1,11 @@
 # Clean and parse circulation from CSV
-# for filename in ../rawData/*-Circulation.csv; do
-#     # Clean data
-#     node circulation-cleaner.js "${filename}"
-# done
+for filename in ../rawData/*-Circulation.csv; do
+    # Clean data
+    node circulation-cleaner.js "${filename}"
+done
 
 # Clean and parse geodata from CSV
-for filename in ../rawData/*-geodata.csv; do
-    # Clean data
-    node geodata-cleaner.js "${filename}"
-done
+# for filename in ../rawData/*-geodata.csv; do
+#     # Clean data
+#     node geodata-cleaner.js "${filename}"
+# done
