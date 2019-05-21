3c6bd0ebb208c8b332a41b0d6909eb98cc52a6d8
Ricky <evholtz@gmail.com>
2019-05-21 13:58:05 -0400

    v0.21 (#62)
    * Fix choropleth logic for resets and center on screen (#59)
    
    * Fix some data issues in cleaners and in the actual render of the data (#61)

--- assets/js/index.js
+++ assets/js/index.js
@@ -276,7 +276,10 @@ const renderCharts = (data) => {
       document.getElementById('renderOption1').checked = false
       document.getElementById('renderOption2').checked = true
       document.getElementById('clearIssueFilterButton').classList.add('hide')
+<<<<<<< HEAD
       document.getElementById('clearGeoFilterButton').classList.add('hide')
+=======
+>>>>>>> 47c1540... v0.21 (#62)
       state.us1ChartRenderOption = 'percentOfPopulation'
       renderIssueData()
       renderGeoData(null, state)
