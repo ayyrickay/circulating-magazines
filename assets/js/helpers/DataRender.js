81e858c8a5a884f7a8597584789ae96f0445a6c7
Ricky <evholtz@gmail.com>
2020-02-28 16:29:56 +0100

    Feature/multi source charts (#83)
    * Load multiple data sets
    
    * Render multi-line chart
    
    * Tentative first go at multi-source charts
    
    * Fix problems with choropleth rendering
    
    * cleanup

--- assets/js/helpers/DataRender.js
+++ assets/js/helpers/DataRender.js
@@ -12,46 +12,47 @@ export function togglePropertyVisibility(key, value, transform) {
   }
 }
 
-export function renderIssueData(data) {
+export function renderIssueData(data, title) {
+  // console.log('Helper logging data', data)
   if (data) {
     const {date, issue_circulation, publishing_company, price, editor, magazine_title, titles_included, circulation_quality, special_notes} = prettifyIssueData(data)
-    document.getElementById('total-circulation').textContent = issue_circulation
-    document.getElementById('issue-date').textContent = date
-    document.getElementById('issue-publisher').textContent = publishing_company
-    document.getElementById('issue-price').textContent = price
-    document.getElementById('issue-editor').textContent = editor
-    document.getElementById('circulation-quality').textContent = `(${circulation_quality})`
-    document.getElementById('titles-included').textContent = titles_included
-    togglePropertyVisibility('special-note', special_notes)
+    document.getElementById(`${title}-total-circulation`).textContent = issue_circulation
+    document.getElementById(`${title}-issue-date`).textContent = date
+    document.getElementById(`${title}-issue-publisher`).textContent = publishing_company
+    document.getElementById(`${title}-issue-price`).textContent = price
+    document.getElementById(`${title}-issue-editor`).textContent = editor
+    document.getElementById(`${title}-circulation-quality`).textContent = `(${circulation_quality})`
+    document.getElementById(`${title}-titles-included`).textContent = titles_included
+    togglePropertyVisibility(`${title}-special-note`, special_notes)
   } else {
-    document.getElementById('circulation-quality').textContent = ''
-    document.getElementById('total-circulation').textContent = '-'
-    document.getElementById('issue-date').textContent = '-'
-    document.getElementById('issue-publisher').textContent = '-'
-    document.getElementById('issue-price').textContent = '-'
-    document.getElementById('issue-editor').textContent = '-'
-    togglePropertyVisibility('special-note')
+    document.getElementById(`${title}-circulation-quality`).textContent = ''
+    document.getElementById(`${title}-total-circulation`).textContent = '-'
+    document.getElementById(`${title}-issue-date`).textContent = '-'
+    document.getElementById(`${title}-issue-publisher`).textContent = '-'
+    document.getElementById(`${title}-issue-price`).textContent = '-'
+    document.getElementById(`${title}-issue-editor`).textContent = '-'
+    togglePropertyVisibility(`${title}-special-note`)
   }
 }
 
-export function renderGeoData(data, state, selectedItem) {
+export function renderGeoData(data, state, title, selectedItem) {
   if (data && state.circulationClicked && selectedItem) {
     const {key, value: {date_counts, sampled_total_sales, sampled_mail_subscriptions, sampled_single_copy_sales, state_population}} = selectedItem
-    document.getElementById('selected-state').textContent = key
-    document.getElementById('mail-subscriptions').textContent = `${renderNumberWithCommas(sampled_mail_subscriptions)}`
-    document.getElementById('single-copy-sales').textContent = `${renderNumberWithCommas(sampled_single_copy_sales)}`
-    document.getElementById('state-circulation').textContent = `${renderNumberWithCommas(sampled_total_sales)}`
-    document.getElementById('state-pop').textContent = `${(sampled_total_sales/state_population * 100).toFixed(3)}%`
-    document.getElementById('percent-of-total').textContent = `${(sampled_total_sales/state.totalSalesByState.value.sampled_total_sales * 100).toFixed(3)}%`
-    document.getElementById('geo-issue-date').textContent = moment.utc(+Object.keys(date_counts)[0]).format('MMM D, YYYY')
+    document.getElementById(`${title}-selected-state`).textContent = key
+    document.getElementById(`${title}-mail-subscriptions`).textContent = `${renderNumberWithCommas(sampled_mail_subscriptions)}`
+    document.getElementById(`${title}-single-copy-sales`).textContent = `${renderNumberWithCommas(sampled_single_copy_sales)}`
+    document.getElementById(`${title}-state-circulation`).textContent = `${renderNumberWithCommas(sampled_total_sales)}`
+    document.getElementById(`${title}-state-pop`).textContent = `${(sampled_total_sales/state_population * 100).toFixed(3)}%`
+    document.getElementById(`${title}-percent-of-total`).textContent = `${(sampled_total_sales/state.totalSalesByState.value.sampled_total_sales * 100).toFixed(3)}%`
+    document.getElementById(`${title}-geo-issue-date`).textContent = moment.utc(+Object.keys(date_counts)[0]).format('MMM D, YYYY')
   } else {
-    document.getElementById('selected-state').textContent = '-'
-    document.getElementById('mail-subscriptions').textContent = '-'
-    document.getElementById('single-copy-sales').textContent = '-'
-    document.getElementById('state-circulation').textContent = '-'
-    document.getElementById('state-pop').textContent = '-'
-    document.getElementById('percent-of-total').textContent = '-'
-    document.getElementById('geo-issue-date').textContent = '-'
+    document.getElementById(`${title}-selected-state`).textContent = '-'
+    document.getElementById(`${title}-mail-subscriptions`).textContent = '-'
+    document.getElementById(`${title}-single-copy-sales`).textContent = '-'
+    document.getElementById(`${title}-state-circulation`).textContent = '-'
+    document.getElementById(`${title}-state-pop`).textContent = '-'
+    document.getElementById(`${title}-percent-of-total`).textContent = '-'
+    document.getElementById(`${title}-geo-issue-date`).textContent = '-'
 
   }
 }
