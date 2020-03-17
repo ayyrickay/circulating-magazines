1d3d78fe680d396cd398a4cc83ebc284e6c00d67
Ricky <evholtz@gmail.com>
2020-03-17 12:10:09 -0700

    Add new titles and appropriate scripts (#89)

--- assets/data/scripts/list-of-title-generator.js
+++ assets/data/scripts/list-of-title-generator.js
@@ -1,13 +1,16 @@
-const path = require('path')
-const fs = require('fs')
+import path from 'path'
+import fs from 'fs'
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const titles = fs.readdirSync('../clean').filter(file => file.indexOf('circulation') > -1)
+const titles = fs.readdirSync(path.join(__dirname, '../clean')).filter(file => file.indexOf('circulation') > -1)
 console.log(titles)
 
 const data = []
 
 for (let i = 0; i < titles.length; i++) {
-  const json = JSON.parse(fs.readFileSync(`../clean/${titles[i]}`))
+  const json = JSON.parse(fs.readFileSync(path.join(__dirname, `../clean/${titles[i]}`)))
   const title_code = titles[i].split('-')[0]
   const title = json[5]
 
@@ -15,7 +18,7 @@ for (let i = 0; i < titles.length; i++) {
     const name = title.canonical_title
     data.push({value: title_code.toLowerCase(), label: name})
   } else {
-    console.log('magazine title is undefined for', json[4])
+    console.log('magazine title is undefined for', json[4], title_code)
   }
 
 }
