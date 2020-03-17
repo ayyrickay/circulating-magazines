b34c777a9a9c48a150cf62657760b91e0ee344bc
Ricky Holtz <evholtz@gmail.com>
2020-03-17 12:30:30 -0700

    Improve bash scripts and interaction with node

--- assets/data/scripts/circulation_cleaner.sh
+++ assets/data/scripts/circulation_cleaner.sh
@@ -1,4 +1,7 @@
 # Clean and parse circulation from CSV
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
 for filename in ../rawData/*-Circulation.csv; do
     # Clean data
     node circulation-cleaner.js "${filename}"
